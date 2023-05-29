5fc1c68151f6e1a2975507e60d7ca25e75b04d90
YanniZ06 <102467588+YanniZ06@users.noreply.github.com>
2023-05-29 21:18:52 +0200

    Mp3, ogg and Wav decoding support
    NOTE: stb_ogg_sound & format need to be set as dependencies
    
    NOTE 2: had to mildly modify the decoders, to make them not error but will obviously credit original author anyways!!

---
+++ src/decoder/Mp3Decoder.hx
@@ -0,0 +1,145 @@
+/*
+	* Belongs to @starburst997 on github,
+	* from repository "https://github.com/notessimo-archive/audio-decoder/tree/master"
+
+	* Under the following license:
+	* MIT License
+
+	* Copyright (c) 2017 Notessimo
+
+	* Permission is hereby granted, free of charge, to any person obtaining a copy
+	* of this software and associated documentation files (the "Software"), to deal
+	* in the Software without restriction, including without limitation the rights
+	* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	* copies of the Software, and to permit persons to whom the Software is
+	* furnished to do so, subject to the following conditions:
+
+	* The above copyright notice and this permission notice shall be included in all
+	* copies or substantial portions of the Software.
+
+	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	* SOFTWARE.
+ */
+
+package decoder;
+
+import format.mp3.Data;
+import format.mp3.Tools;
+import haxe.io.Bytes;
+import haxe.io.BytesInput;
+
+/**
+ * Simple interface to MP3 Decoder
+ *
+ * Progressively decode the MP3 by requesting range
+ * 
+ * JS works but sample point are wayyyy off compared to Flash extract()
+ *
+ * Will need some major cleanup, mostly experimenting right now...
+ */
+class Mp3Decoder extends Decoder
+{
+	// Constructor
+	public function new(bytes:Bytes, delay:Bool = false)
+	{
+		super(bytes, delay);
+	}
+
+	override function create()
+	{
+		trace("");
+
+		var info = Mp3Utils.getInfo(bytes);
+		trace("MP3 Reader finished");
+
+		_process(info.length, info.channels, info.sampleRate);
+	}
+
+	// Read samples inside the MP3
+	private override function read(start:Int, end:Int):Bool
+	{
+		// TODO: !!! WDYM???? TODO???? I THOUGHT THIS WAS FINISHED NOOO
+		return true;
+	}
+}
+
+// Modify the MP3 Class a bit so we can get the information a bit more efficiently
+typedef Mp3Info =
+{
+	var sampleRate:Int;
+	var channels:Int;
+	var length:Int;
+};
+
+private class Mp3Utils extends format.mp3.Reader
+{
+	var bi:BytesInput;
+	var channels:Int = 1;
+	var sampleRate:Int = 44100;
+
+	public function new(i:BytesInput)
+	{
+		bi = i;
+
+		super(i);
+	}
+
+	public override function readFrame():MP3Frame
+	{
+		var header = readFrameHeader();
+
+		if (header == null || Tools.isInvalidFrameHeader(header))
+			return null;
+
+		channels = header.channelMode == Mono ? 1 : 2;
+
+		sampleRate = switch (header.samplingRate)
+		{
+			case SR_48000: 48000;
+			case SR_44100: 44100;
+			case SR_32000: 32000;
+			case SR_24000: 24000;
+			case SR_22050: 22050;
+			case SR_12000: 12000;
+			case SR_11025: 11025;
+			case SR_8000: 8000;
+			default: 44100;
+		};
+
+		try
+		{
+			var length = Tools.getSampleDataSizeHdr(header);
+			samples += Tools.getSampleCountHdr(header);
+			sampleSize += length;
+
+			bi.position += length;
+
+			return {
+				header: header,
+				data: null
+			};
+		}
+		catch (e:haxe.io.Eof)
+		{
+			return null;
+		}
+	}
+
+	public static function getInfo(bytes:Bytes):Mp3Info
+	{
+		var reader = new Mp3Utils(new BytesInput(bytes));
+
+		reader.readFrames();
+
+		return {
+			sampleRate: reader.sampleRate,
+			channels: reader.channels,
+			length: reader.samples
+		};
+	}
+}
