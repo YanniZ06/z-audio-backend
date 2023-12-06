0868b818eec11a2c594d6d03547e2a82b3e91816
YanniZ06 <102467588+YanniZ06@users.noreply.github.com>
2023-12-07 00:34:16 +0100

    Fixed compilation, started work on streaming
    - stream object folder created
    - started on base stream interface
    - deleted the cpp folder and its related macro (too much work for too little payoff, guess mp3decoder is doomed to forever look ugly as NOTHING ELSE WORKS!!)

---
+++ src/zAudio/file_stream/IStreamBase.hx
@@ -0,0 +1,24 @@
+package zAudio.file_stream;
+
+import haxe.ds.Vector;
+import haxe.io.Bytes;
+/**
+ * Interface that defines shared functions between multiple different file-streams
+ */
+interface IStreamBase {
+    // Processed chunks stored, older data indexed lower than "processLimit", current running chunk being indexed at "processLimit" and newer data indexed over "processLimit"
+    public var processedData:Vector<Bytes>;
+
+    // Same as the above but for reverse data (eg. chunk representing 00:01 in a 60 second song would be a chunk representing 00:59 here)
+    public var processedReverseData:Vector<Bytes>;
+
+     // Will define the maximum number of processedData to be stored at once, into one direction (same number is used for other direction aswell)
+    public var processLimit(default, set):cpp.UInt8;
+
+    // Specify size of processed chunks, shouldnt be larger than like 2 seconds of audio I think (value is defined in bytes i think)
+    // Value can only be set once upon stream creation
+    public var proccessLength(default, null):Int;
+
+    // Heavily WIP
+    public function seek(pos:Int):Void;
+}
---
+++ src/zAudio/file_stream/WAVEStream.hx
@@ -0,0 +1,12 @@
+package zAudio.file_stream;
+
+import haxe.io.BytesInput;
+
+/**
+ * Class that creates a WAVE-file(.wav) streaming interface
+ * 
+ * https://api.haxe.org/haxe/io/Input.html#readBytes
+ */
+class WAVEStream {
+    public var fileInput:BytesInput;
+}
